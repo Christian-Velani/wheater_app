3caf8d490dd136c63497d51671446042c05d2b84
Christian Velani <chris13082000@gmail.com>
2023-10-06 11:14:01 -0300

    Coloquei a localização e ajustei o sistema do forecast

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'style.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
@@ -11,12 +12,37 @@ void main() {
 }
 
 class WheaterApp extends StatelessWidget {
-  Future<Map<String, dynamic>?> loadWeatherData() async {
+  Future<Map<String, dynamic>?> loadWeatherData(
+      {required BuildContext context}) async {
     var queryParams = {
       "key": "81134a81c96740db958105843232808",
       "lang": "pt",
     };
+    // Solicita permissão de localização
+    LocationPermission permission = await Geolocator.requestPermission();
+
+    // Se a permissão for negada, mostra uma mensagem de erro e fecha o aplicativo
+    if (permission == LocationPermission.denied) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Erro'),
+          content: Text('O usuário negou a permissão de localização.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                SystemNavigator.pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+      return null;
+    }
     Position location = await Geolocator.getCurrentPosition();
+    print(location);
 
     queryParams['q'] = "${location.latitude},${location.longitude}";
 
@@ -36,33 +62,39 @@ class WheaterApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xFF255AF4),
         body: FutureBuilder<Map<String, dynamic>?>(
-            future: loadWeatherData(),
+            future: loadWeatherData(context: context),
             builder: (context, snapshot) {
               if (!snapshot.hasData) {
                 return CircularProgressIndicator();
               }
 
               var dados = snapshot.data!;
-              var forecastday =
-                  dados['forecast']['forecastday'][0]['hour'] as List<dynamic>;
+              var clima = ClimaAPI(
+                  dados['current']['humidity'],
+                  dados['current']['condition']['icon'],
+                  dados['location']['name'],
+                  dados['current']['temp_c'],
+                  dados['current']['wind_kph'],
+                  dados['current']['feelslike_c'],
+                  dados['forecast']['forecastday'][0]['hour'],
+                  dados['current']['condition']['text'],
+                  DateTime.parse(dados['location']['localtime']).hour);
 
               return SafeArea(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Text(dados['location']['name'], style: titleStyle),
+                    Text(clima.name, style: titleStyle),
                     Column(
                       children: [
                         Container(
-                          child: Image.asset('images/01_sunny_color.png'),
                           width: 96,
                           height: 96,
                           margin: EdgeInsets.fromLTRB(0, 0, 0, 24),
+                          child: Image.network('https:${clima.icone}'),
                         ),
-                        Text(dados['current']['condition']['text'],
-                            style: titleStyle),
-                        Text("${dados['current']['temp_c']}°C",
-                            style: temperatureStyle),
+                        Text(clima.climanome, style: titleStyle),
+                        Text("${clima.temp_c}°C", style: temperatureStyle),
                       ],
                     ),
                     Container(
@@ -74,24 +106,21 @@ class WheaterApp extends StatelessWidget {
                             children: [
                               Image.asset('images/humidity.png'),
                               Text("Humidity", style: iconStyle),
-                              Text("${dados['current']['humidity']}%",
-                                  style: iconStyle),
+                              Text("${clima.humidity}%", style: iconStyle),
                             ],
                           ),
                           Column(
                             children: [
                               Image.asset('images/wind.png'),
                               Text("Wind", style: iconStyle),
-                              Text("${dados['current']['wind_kph']}km/h",
-                                  style: iconStyle),
+                              Text("${clima.wind_kph}km/h", style: iconStyle),
                             ],
                           ),
                           Column(
                             children: [
                               Image.asset('images/feels_like.png'),
                               Text("Feels Like", style: iconStyle),
-                              Text("${dados['current']['feelslike_c']}°C",
-                                  style: iconStyle),
+                              Text("${clima.feelslike_c}°C", style: iconStyle),
                             ],
                           ),
                         ],
@@ -100,13 +129,17 @@ class WheaterApp extends StatelessWidget {
                     Container(
                       height: 100,
                       // margin: EdgeInsets.only(top: 80),
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: forecastday
-                            .map((item) => ForecastDay(
-                                item['time_epoch'], "chuva", item['temp_c']))
-                            .toList(),
-                      ),
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: clima.forecast_hours_late.length,
+                          itemBuilder: (context, index) {
+                            return ForecastDay(
+                              clima.forecast_hours_late[index]['time'],
+                              clima.forecast_hours_late[index]['condition']
+                                  ['icon'],
+                              clima.forecast_hours_late[index]['temp_c'],
+                            );
+                          }),
                     ),
                   ],
                 ),
@@ -118,14 +151,18 @@ class WheaterApp extends StatelessWidget {
 }
 
 class ForecastDay extends StatelessWidget {
-  int timeEpoch;
+  String time;
   String image;
   double temperature;
   String? hour;
 
-  ForecastDay(this.timeEpoch, this.image, this.temperature) {
-    var data = DateTime.fromMillisecondsSinceEpoch(timeEpoch);
-    this.hour = data.hour.toString();
+  ForecastDay(
+    this.time,
+    this.image,
+    this.temperature,
+  ) {
+    var data = DateTime.parse(time);
+    hour = data.hour.toString();
   }
 
   @override
@@ -137,10 +174,32 @@ class ForecastDay extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(hour!, style: hourStyle),
-          Image.asset('images/$image.png', width: 36, height: 36),
+          Image.network('https:${image}', width: 36, height: 36),
           Text("$temperature°", style: hourTemperatureStyle)
         ],
       ),
     );
+  }
+}
+
+class ClimaAPI {
+  String name;
+  double temp_c;
+  String icone;
+  double wind_kph;
+  int humidity;
+  double feelslike_c;
+  List forecast_hours;
+  String climanome;
+  int hour;
+  List forecast_hours_late = [];
+
+  ClimaAPI(this.humidity, this.icone, this.name, this.temp_c, this.wind_kph,
+      this.feelslike_c, this.forecast_hours, this.climanome, this.hour) {
+    for (int i = 0; i < forecast_hours.length; i++) {
+      if (DateTime.parse(forecast_hours[i]['time']).hour > hour) {
+        forecast_hours_late.add(forecast_hours[i]);
+      }
+    }
   }
 }
